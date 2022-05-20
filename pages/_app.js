05325f6619e6b340f4eb1e78a1e8bbca9adb8092
rgalusha <robbiegalusha@gmail.com>
2022-05-20 13:58:40 -0400

    escape error

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,21 @@ function MyApp({ Component, pageProps }) {
     <>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Platinum Service's Weight Ticket</title>
-        <meta name="description" content="Platinum Service's Weight Ticket" />
+        <title>Platinum Services Weight Ticket</title>
+        <meta name="description" content="Platinum Services Weight Ticket" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+          crossorigin="anonymous"
+        />
       </head>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossOrigin="anonymous"></script>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+        crossOrigin="anonymous"
+      ></script>
       <Component {...pageProps} />
     </>
   );
